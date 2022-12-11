a9c331eee5d4abda156ed080e260c440c79b507c
Murari Thakur <itsmeanimatormt@gmail.com>
2022-12-11 18:37:11 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,76 @@
+// Toggle Theme
+let logoElem=document.getElementById("logo");
+logoElem.addEventListener('click',()=>{
+    if(document.body.getAttribute("theme")==="slateblue-color"){
+        document.body.setAttribute("theme", "red-color");
+    }
+    else{
+        document.body.setAttribute("theme", "slateblue-color");
+    }
+    
+});
+// Toggle View
+let mobileElem=document.getElementById('mobile-icon');
+let navElems=document.getElementById("nav-elems");
+mobileElem.addEventListener('click',()=>{
+    if(mobileElem.classList.contains('fa-bars')){
+        mobileElem.classList.replace("fa-bars","fa-close");
+        navElems.style.left="-40px"
+    }
+    else{
+        mobileElem.classList.replace("fa-close","fa-bars"); 
+        navElems.style.left="-250px";
+    }
+})
+navElems.addEventListener("click",()=>{
+    navElems.style.left="-250px"
+    mobileElem.classList.replace("fa-close","fa-bars"); 
+})
+// About Section
+let tabLinkElem=document.getElementsByClassName('tab-links');
+let tabContentElem=document.getElementsByClassName('tab-contents');
+function opentab(tabname){
+    for(tablink of tabLinkElem){
+      tablink.classList.remove("active-link")
+    }
+    for(tabContent of tabContentElem){
+        tabContent.classList.remove("active-tab")
+      }
+      event.currentTarget.classList.add('active-link');
+      document.getElementById(tabname).classList.add('active-tab');
+}
+
+// Dyanmic text Code
+const dynamicContent=document.getElementById('dynamic-text');
+const phrases=["Web Developer..","A Learner...","Web 3.0 Enthusiast..","A Student"];
+let phraseIndex=0;
+let letterIndex=0;
+function typeLetters(phrase){
+    if(letterIndex==phrase.length){
+        deleteLetters();
+    }
+    else if(letterIndex<phrase.length){
+        dynamicContent.textContent += phrase.charAt(letterIndex);
+        letterIndex+=1;
+        setTimeout(function (){
+            typeLetters(phrase)
+        },300);
+    }
+}
+function deleteLetters(){
+    if(letterIndex==-1){
+        phraseIndex=(phraseIndex+1)%phrases.length;
+        letterIndex=0;
+        typeLetters(phrases[phraseIndex]);
+    }
+    else if(letterIndex>-1){
+        let updatedPhrase="#";
+        for(let index=0;index<letterIndex;index++){
+            updatedPhrase+=phrases[phraseIndex].charAt(index);
+        }
+        dynamicContent.textContent=updatedPhrase;
+        letterIndex-=1;
+        setTimeout(deleteLetters,200);
+    }
+}
+typeLetters(phrases[phraseIndex])
